5b893c11e5894a56d14258b30de4e3e58b92eb11
vxsnth <vasanthanandakumar29@gmail.com>
2025-08-29 21:33:49 +0530

    config.tsx

--- nexride/config.tsx
+++ nexride/config.tsx
@@ -1,2 +1,2 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyC0_i2q6-Zp5q6gJXIbvgzo5GL71A_bXX4";
+export const GOOGLE_MAPS_API_KEY = "";
 export const NGROK_URL = 'https://c0cbde79c4ed.ngrok-free.app';
