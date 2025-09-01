f34292e656ea3ee31aaa4ac5cdae6704a721fe35
vxsnth <vasanthanandakumar29@gmail.com>
2025-09-01 22:59:45 +0530

    changed the api key
     more about pull requests

--- nexride/config.tsx
+++ nexride/config.tsx
@@ -1,2 +1,2 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyC_XW0rl6eDEenXA1fdmaBXOll5x2ybipk";
+export const GOOGLE_MAPS_API_KEY = "";
 export const NGROK_URL = 'https://394f856fe93d.ngrok-free.app';
